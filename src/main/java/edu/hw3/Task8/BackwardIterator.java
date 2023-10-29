95f1fb7c00a1b61b21e32bde6be1b9f88018f817
Donat <konuspaev.dn@phystech.edu>
2023-10-29 23:54:27 +0300

    New commit

--- src/main/java/edu/hw3/Task8/BackwardIterator.java
+++ src/main/java/edu/hw3/Task8/BackwardIterator.java
@@ -1,4 +1,29 @@
 package edu.hw3.Task8;
 
-public class BackwardIterator {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+public class BackwardIterator<E> implements Iterator<E> {
+    private final Stack<E> backwardCollection = new Stack<>();
+
+    public BackwardIterator(Collection<E> collection) {
+        for (E e : collection) {
+            backwardCollection.push(e);
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !backwardCollection.empty();
+    }
+
+    @Override
+    public E next() {
+        if (backwardCollection.empty()) {
+            throw new NoSuchElementException();
+        }
+        return backwardCollection.pop();
+    }
 }
