b8850cf74bd3ffb768fcf6bf47fb337c235da753
Donat <konuspaev.dn@phystech.edu>
2023-11-04 22:22:37 +0300

    New commit

--- src/test/java/edu/hw4/AnimalCrossingTest.java
+++ src/test/java/edu/hw4/AnimalCrossingTest.java
@@ -2,10 +2,21 @@ package edu.hw4;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
 import java.util.ArrayList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AnimalCrossingTest {
+
+    static Arguments[] rectangles() {
+        return new Arguments[]{
+            Arguments.of(),
+            Arguments.of(),
+            Arguments.of(),
+            Arguments.of(),
+            Arguments.of()
+        };
+    }
 
     @Test
     @DisplayName("Sort by height")
