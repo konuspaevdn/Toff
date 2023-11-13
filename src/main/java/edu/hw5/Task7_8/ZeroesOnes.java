5267956816bdc5928faf186ead707fe597fd4f84
donat <konuspaev.dn@phystech.edu>
2023-11-14 02:47:18 +0300

    New commit

--- src/main/java/edu/hw5/Task7_8/ZeroesOnes.java
+++ src/main/java/edu/hw5/Task7_8/ZeroesOnes.java
@@ -15,7 +15,7 @@ public class ZeroesOnes {
     }
 
     public static Boolean checkSameFirstAndLastS(String str) {  // Task7
-        Pattern pattern = Pattern.compile("(^0[01]*0$)?(^1[01]*1$)");
+        Pattern pattern = Pattern.compile("((^0[01]*0$)|(^1[01]*1$))");
         Matcher matcher = pattern.matcher(str);
         return matcher.find();
     }
@@ -24,5 +24,47 @@ public class ZeroesOnes {
         Pattern pattern = Pattern.compile("^[01]{1,3}$");
         Matcher matcher = pattern.matcher(str);
         return matcher.find();
+    }
+
+    public static Boolean checkOddLength(String str) {  // Task8
+        Pattern pattern = Pattern.compile("^[01]([01][01])*$");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
+
+    public static Boolean checkFirstSAndLength(String str) {  // Task8
+        Pattern pattern = Pattern.compile("((^0([01][01])*$)|(^1[01]([01][01])*$))");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
+
+    public static Boolean checkZeroesNumMultipleOf3(String str) {  // Task8
+        Pattern pattern = Pattern.compile("^1*(01*01*01*)*$");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
+
+    public static Boolean checkForForbiddenStrings(String str) {  // Task8
+        Pattern pattern = Pattern.compile("^((11)|(111))$");
+        Matcher matcher = pattern.matcher(str);
+        return !matcher.find();
+    }
+
+    public static Boolean checkOddSIsOne(String str) {  // Task8
+        Pattern pattern = Pattern.compile("^(1[01])*1?$");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
+
+    public static Boolean checkTwoZeroesOneOne(String str) {  // Task8
+        Pattern pattern = Pattern.compile("((^00+1?$)|(^100+$)|(^0+10+$))");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
+
+    public static Boolean checkForConsequentialOnes(String str) {  // Task8
+        Pattern pattern = Pattern.compile("[01]*11");
+        Matcher matcher = pattern.matcher(str);
+        return !matcher.find();
     }
 }
--- src/test/java/edu/hw5/ZeroesOnesTest.java
+++ src/test/java/edu/hw5/ZeroesOnesTest.java
@@ -31,5 +31,80 @@ public class ZeroesOnesTest {
         assertThat(ZeroesOnes.checkLengthBetween1and3(str)).isTrue();
         str = "0101";
         assertThat(ZeroesOnes.checkLengthBetween1and3(str)).isFalse();
+
+        str = "1";
+        assertThat(ZeroesOnes.checkOddLength(str)).isTrue();
+        str = "10";
+        assertThat(ZeroesOnes.checkOddLength(str)).isFalse();
+        str = "001";
+        assertThat(ZeroesOnes.checkOddLength(str)).isTrue();
+        str = "0011";
+        assertThat(ZeroesOnes.checkOddLength(str)).isFalse();
+
+        str = "01";
+        assertThat(ZeroesOnes.checkFirstSAndLength(str)).isFalse();
+        str = "101";
+        assertThat(ZeroesOnes.checkFirstSAndLength(str)).isFalse();
+        str = "01001";
+        assertThat(ZeroesOnes.checkFirstSAndLength(str)).isTrue();
+        str = "1111";
+        assertThat(ZeroesOnes.checkFirstSAndLength(str)).isTrue();
+
+        str = "";
+        assertThat(ZeroesOnes.checkZeroesNumMultipleOf3(str)).isTrue();
+        str = "1111";
+        assertThat((ZeroesOnes.checkZeroesNumMultipleOf3(str))).isTrue();
+        str = "1011";
+        assertThat(ZeroesOnes.checkZeroesNumMultipleOf3(str)).isFalse();
+        str = "01101";
+        assertThat(ZeroesOnes.checkZeroesNumMultipleOf3(str)).isFalse();
+        str = "11110";
+        assertThat(ZeroesOnes.checkZeroesNumMultipleOf3(str)).isFalse();
+        str = "101001";
+        assertThat(ZeroesOnes.checkZeroesNumMultipleOf3(str)).isTrue();
+        str = "00100";
+        assertThat(ZeroesOnes.checkZeroesNumMultipleOf3(str)).isFalse();
+
+        str = "";
+        assertThat(ZeroesOnes.checkForForbiddenStrings(str)).isTrue();
+        str = "0";
+        assertThat(ZeroesOnes.checkForForbiddenStrings(str)).isTrue();
+        str = "11";
+        assertThat(ZeroesOnes.checkForForbiddenStrings(str)).isFalse();
+        str = "110";
+        assertThat(ZeroesOnes.checkForForbiddenStrings(str)).isTrue();
+        str = "111";
+        assertThat(ZeroesOnes.checkForForbiddenStrings(str)).isFalse();
+        str = "1110";
+        assertThat(ZeroesOnes.checkForForbiddenStrings(str)).isTrue();
+
+        str = "";
+        assertThat(ZeroesOnes.checkOddSIsOne(str)).isTrue();
+        str = "0";
+        assertThat(ZeroesOnes.checkOddSIsOne(str)).isFalse();
+        str = "11";
+        assertThat(ZeroesOnes.checkOddSIsOne(str)).isTrue();
+        str = "101";
+        assertThat(ZeroesOnes.checkOddSIsOne(str)).isTrue();
+        str = "11100";
+        assertThat(ZeroesOnes.checkOddSIsOne(str)).isFalse();
+
+        str = "011";
+        assertThat(ZeroesOnes.checkTwoZeroesOneOne(str)).isFalse();
+        str = "1";
+        assertThat(ZeroesOnes.checkTwoZeroesOneOne(str)).isFalse();
+        str = "1001";
+        assertThat(ZeroesOnes.checkTwoZeroesOneOne(str)).isFalse();
+        str = "010";
+        assertThat(ZeroesOnes.checkTwoZeroesOneOne(str)).isTrue();
+        str = "00010";
+        assertThat(ZeroesOnes.checkTwoZeroesOneOne(str)).isTrue();
+
+        str = "11";
+        assertThat(ZeroesOnes.checkForConsequentialOnes(str)).isFalse();
+        str = "001110";
+        assertThat(ZeroesOnes.checkForConsequentialOnes(str)).isFalse();
+        str = "010001";
+        assertThat(ZeroesOnes.checkForConsequentialOnes(str)).isTrue();
     }
 }
